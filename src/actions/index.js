fb99449c0cf74499939546be2adee3f3b1a99437
uidevperson <uidevthing@gmail.com>
2017-12-02 13:19:21 +0000

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ export function selectBook(book) {
 	// selectbook is an action creator
 	// it needs to return an action
 	// which is an object with type property
-	
 	return {
 		type: 'BOOK_SELECTED',
 		payload: book,
