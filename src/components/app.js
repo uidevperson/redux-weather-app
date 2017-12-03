8d05595f51c45f991c7e3a4cf8882e0d1131982f
uidevperson <uidevthing@gmail.com>
2017-12-03 18:14:02 +0000

    searchable container init

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import BookList from '../containers/book-list';
-import BookDetail from '../containers/book-detail';
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      	<BookList />
-      	<BookDetail />
+      	<SearchBar />
       </div>
     );
   }
