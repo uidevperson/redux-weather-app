8e1d06bb1efdd3dc8f064e86c80f39ba0d9ac51c
uidevperson <uidevthing@gmail.com>
2017-12-03 18:31:38 +0000

    controlled component and binding context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,11 +3,26 @@ import React, {Component} from 'react';
 // import {bindActionCreators} from 'redux';
 
 class SearchBar extends Component {
+	constructor(props){
+		super(props);
+		this.state = {term: ''};
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(e) {
+		console.log(e.target.value);
+		this.setState({term: e.target.value});
+	}
 
 	render(){
 		return (
 			<form className="input-group">
-				<input />
+				<input
+					placeholder="get a 5 day forcast for your favourite city"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				 />
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
