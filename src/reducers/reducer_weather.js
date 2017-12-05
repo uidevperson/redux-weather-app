f675acc561526807fc17ec34610372950c5729f4
uidevperson <uidevthing@gmail.com>
2017-12-05 20:58:51 +0000

    weather list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,11 +1,10 @@
 // state arguement is not application state
-import { FETCH_WEATHER } from '../actions.index';
+import { FETCH_WEATHER } from '../actions/index';
 // only the state this reducer is responsile for
 export default function(state = [], action) {
 	switch(action.type) {
 		case FETCH_WEATHER:
 			return [action.payload.data, ...state]; //[city, city, city]
-			// return state.concat[action.payload.data]; es5 version of above line
 	}
 	return state;
 }
