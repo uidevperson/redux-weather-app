27fc232830785b71e7591eaf29d2128c64941ab5
uidevperson <uidevthing@gmail.com>
2017-12-05 20:34:00 +0000

    update reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,11 @@
 // state arguement is not application state
+import { FETCH_WEATHER } from '../actions.index';
 // only the state this reducer is responsile for
-export default function(state = null, action) {
-	// switch(action.type) {
-	// 	case 'WEATHER':
-	// 		return action.payload;
-	// }
+export default function(state = [], action) {
+	switch(action.type) {
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state]; //[city, city, city]
+			// return state.concat[action.payload.data]; es5 version of above line
+	}
 	return state;
 }
