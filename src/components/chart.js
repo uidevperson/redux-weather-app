708d94f97c790ec931d688ddb6e008fb5e86c389
uidevperson <uidevthing@gmail.com>
2017-12-06 21:32:54 +0000

    update

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ export default (props) => {
 
     return (
       <div>
-      	<Sparklines height={120} width={180} data={props.data}>
+      	<Sparklines height={120} width={180} data={props.data} >
       	  <SparklinesLine color={props.color} />
       	</Sparklines>
       </div>
